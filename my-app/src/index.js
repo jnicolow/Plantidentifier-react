e24864a15f725278e8840be4ee84d3cda80c0dea
jcnicolowUH <jnicolow@hawaii.edu>
2023-03-02 06:58:09 -1000

    Task 4

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,33 +23,32 @@ import {ChatQuote} from 'react-bootstrap-icons';
 import { ReactComponent as IconMenu } from './plantnet-logo.svg'
 
 const TopMenu = () => (
-  <h1> Plant Identifier</h1>
-  // <Navbar bg="light" expand="lg">
-  //   <Container>
-  //     <Nav className="me-auto">
-  //       <IconMenu width="10rem"></IconMenu>
-  //     </Nav>
-  //     <Nav className="justify-content-end">
-  //       <Nav.Link><Search/>Identify</Nav.Link>
-  //       <Container className="d-inline-flex">
-  //         <Nav.Link className="mx-2"><FilterRight/>Explore</Nav.Link>
-  //         <Nav.Link className="mx-2"><People/>Contributions</Nav.Link>
-  //         <Nav.Link className="mx-2"><PeopleFill/>Groups</Nav.Link>
-  //       </Container>
-  //       <Nav.Link><GeoAltFill/>Geo<strong>Beta</strong></Nav.Link>
-  //
-  //
-  //       <NavDropdown title={<><ChatQuote/>English</>}>
-  //       </NavDropdown>
-  //     </Nav>
-  //
-  //     <Nav className="justify-content-end">
-  //       <Nav.Link><Facebook/></Nav.Link>
-  //       <Nav.Link><Twitter/></Nav.Link>
-  //       <Nav.Link><Instagram/></Nav.Link>
-  //     </Nav>
-  //   </Container>
-  // </Navbar>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Nav className="me-auto">
+        <IconMenu width="10rem"></IconMenu>
+      </Nav>
+      <Nav className="justify-content-end">
+        <Nav.Link><Search/>Identify</Nav.Link>
+        <Container className="d-inline-flex">
+          <Nav.Link className="mx-2"><FilterRight/>Explore</Nav.Link>
+          <Nav.Link className="mx-2"><People/>Contributions</Nav.Link>
+          <Nav.Link className="mx-2"><PeopleFill/>Groups</Nav.Link>
+        </Container>
+        <Nav.Link><GeoAltFill/>Geo<strong>Beta</strong></Nav.Link>
+
+
+        <NavDropdown title={<><ChatQuote/>English</>}>
+        </NavDropdown>
+      </Nav>
+
+      <Nav className="justify-content-end">
+        <Nav.Link><Facebook/></Nav.Link>
+        <Nav.Link><Twitter/></Nav.Link>
+        <Nav.Link><Instagram/></Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
